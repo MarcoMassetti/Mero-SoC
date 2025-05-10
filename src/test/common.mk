a04e7595c280e891f7f40cf45c9741f60dd2f81c
MarcoMassetti <marco.massetti@protonmail.com>
2025-05-10 15:30:54 +0200

    Cleaned compilation/simulation flow

--- src/test/common.mk
+++ src/test/common.mk
@@ -1,44 +1,47 @@
-include /media/marco/Data_L/EDA/openlane2/my_designs/cpu/base.mk
-
-export CURRENT_DIR=$(shell pwd)
-
-
-S_FILES := $(wildcard ./S/*.S)
-export SOURCE_FILE_NAME ?= add
-
+# Variables used during simulation
 export OUTPUT_DIR
 export TEST_DIR
 
-# Output folder for testcase
+# Simulation directory for testcase
 OUTPUT_DIR := $(subst $(SRC_DIR),$(OBJ_DIR),$(CURRENT_DIR))
 
+# Start simulation in batch mode
 batch : analyze-design analyze-testbench golden
 	cd $(OUTPUT_DIR) ; vsim $(WORK_DIR).cpu_top_tb -l tc.out -quiet -batch -do $(TEST_DIR)/common_sim.tcl -g/cpu_top_tb/inst_ram_wrapper/inst_ram/FILE_NAME=$(OUTPUT_DIR)/$(SOURCE_FILE_NAME).bin
 	
-	
+# Start simulation in gui mode
 gui : analyze-design analyze-testbench  golden
 	cd $(OUTPUT_DIR) ; vsim $(WORK_DIR).cpu_top_tb -l tc.out -quiet -do $(TEST_DIR)/common_sim.tcl -g/cpu_top_tb/inst_ram_wrapper/inst_ram/FILE_NAME=$(OUTPUT_DIR)/$(SOURCE_FILE_NAME).bin
 	
-	
+# Compile the design
 analyze-design :
 	make -f $(SRC_DIR)/design/Makefile analyze
-	
+
+# Compile the testbench
 analyze-testbench :
 	make -f $(SRC_DIR)/testbench/Makefile analyze
 	
-	
+# Expected results dependency
 golden : $(OUTPUT_DIR)/$(SOURCE_FILE_NAME)_rf_golden.txt
 
+# Calculate the expected results with the ripes simulator
 $(OUTPUT_DIR)/$(SOURCE_FILE_NAME)_rf_golden.txt : $(OUTPUT_DIR)/$(SOURCE_FILE_NAME).bin
 	ripes --mode cli --proc "RV32_5S" --timeout 60000 --src $< -t bin --regs --output $@
 	
-$(OUTPUT_DIR)/$(SOURCE_FILE_NAME).bin : ./S/$(SOURCE_FILE_NAME).S ./start.S ./S/riscv_test.h ./S/test_macros.h ./Makefile $(OUTPUT_DIR)
-	$(ARCH)-gcc $(OPTS) ./start.S $< -o $(OUTPUT_DIR)/$(SOURCE_FILE_NAME).elf -DTEST_FUNC_NAME=$(SOURCE_FILE_NAME) -DTEST_FUNC_TXT='"$(SOURCE_FILE_NAME)"' -DTEST_FUNC_RET=$(SOURCE_FILE_NAME)_ret -DMY_TEST_$(SOURCE_FILE_NAME)
+# Compile C code and produce binary file
+$(OUTPUT_DIR)/$(SOURCE_FILE_NAME).bin : $(SOURCE_FILE_NAME).c $(OUTPUT_DIR)
+	$(ARCH)-gcc $(OPTS) $(CRT0) $< -o $(OUTPUT_DIR)/$(SOURCE_FILE_NAME).elf
 	$(ARCH)-objcopy $(OUTPUT_DIR)/$(SOURCE_FILE_NAME).elf $@ -O binary
-	
+
+# Creating simulation directory
 $(OUTPUT_DIR) : 
 	mkdir -p $(OUTPUT_DIR)
 
+# Print content of the binary file
 dump : $(OUTPUT_DIR)/$(SOURCE_FILE_NAME).elf
 	$(ARCH)-objdump -d $(OUTPUT_DIR)/$(SOURCE_FILE_NAME).elf
-	
+
+
+
+
+
--- src/test/procedures.tcl
+++ src/test/procedures.tcl
@@ -1,4 +1,4 @@
-
+# Load binary file into testbench ram
 proc load_code {} {
 	set input_file "$::env(OBJ_DIR)/$::env(SOURCE_FILE_NAME).bin"
 	set mem_path "sim:/cpu_top_tb/inst_ram_wrapper/inst_ram/mem"
@@ -14,17 +14,15 @@ proc load_code {} {
 		binary scan $bin_data i data
 		set data [format "%x" $data]
 		append cmd " $mem_path\($addr) $data"
-		#echo "$addr: $data"
 		incr addr
 		
 		set bin_data [read $fp 4]
 	}
-
-	#echo $cmd
 	eval $cmd
 	close $fp
 }
 
+# Run until trap is issued or until timeout
 proc run_until_break {max_time} {
 	onbreak resume
 	
@@ -44,12 +42,12 @@ proc run_until_break {max_time} {
 		stop
 	}
 	
-	run -all
-	
+	run -all	
 }
 
+# Print register file content
 proc report_rf {} {
-	set output_file "$::env(OBJ_DIR)/$::env(SOURCE_FILE_NAME)_rf_dut.txt"
+	set output_file "$::env(OUTPUT_DIR)/$::env(SOURCE_FILE_NAME)_rf_dut.txt"
 	set rf_path "sim:/cpu_top_tb/DUT/inst_cpu/inst_register_file/registers"
 	
 	set fp [open $output_file w]  
@@ -67,8 +65,9 @@ proc report_rf {} {
 	close $fp	
 }
 
+# Compare register file cotent with expected result
 proc compare_rf_content {} {
-	if {[catch {exec diff $::env(OBJ_DIR)/$::env(SOURCE_FILE_NAME)_rf_dut.txt $::env(OBJ_DIR)/$::env(SOURCE_FILE_NAME)_rf_golden.txt} fid]} {
+	if {[catch {exec diff $::env(OUTPUT_DIR)/$::env(SOURCE_FILE_NAME)_rf_dut.txt $::env(OUTPUT_DIR)/$::env(SOURCE_FILE_NAME)_rf_golden.txt} fid]} {
 		add message -noident -severity error "RF content differs\n$::errorInfo"
 	} else {
 		add message -noident -severity note "RF content matches"
